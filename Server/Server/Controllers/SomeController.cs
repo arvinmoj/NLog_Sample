befcee7b9e5496ad0b7d25c0a55781481e82e6f2
Arvinmoj <arvinmojaverian@hotmail.com>
2022-08-14 11:13:54 +0430

    NLog

---
+++ Server/Server/Controllers/SomeController.cs
@@ -0,0 +1,51 @@
+﻿namespace Server.Controllers
+{
+	[Microsoft.AspNetCore.Mvc.ApiController]
+	[Microsoft.AspNetCore.Mvc.Route("[controller]")]
+	public class SomeController : Microsoft.AspNetCore.Mvc.Controller
+	{
+		//public SomeController
+		//	(Microsoft.Extensions.Logging.ILogger<SomeController> logger) : base()
+		//{
+		//	Logger = logger;
+		//}
+
+		//protected Microsoft.Extensions.Logging.ILogger<SomeController> Logger { get; }
+
+		public SomeController
+			(Logging.ILogger<SomeController> logger) : base()
+		{
+			Logger = logger;
+		}
+
+		protected Logging.ILogger<SomeController> Logger { get; }
+
+		[Microsoft.AspNetCore.Mvc.HttpGet]
+		public Microsoft.AspNetCore.Mvc.IActionResult Get()
+		{
+			// **************************************************
+			//Logger.LogTrace(message: "Trace 1");
+			//Logger.LogDebug(message: "Debug 1");
+			//Logger.LogInformation(message: "Info 1");
+			//Logger.LogWarning(message: "Warn 1");
+			//Logger.LogError(message: "Error 1");
+			//Logger.LogCritical(message: "Fatal 1");
+			// **************************************************
+
+			// **************************************************
+			System.Exception innerException = new System.Exception(message: "Inner Exception Message (1)");
+			System.Exception exception = new System.Exception(message: "Exception Message (1)", innerException: innerException);
+
+			System.Collections.Hashtable hashtable = new System.Collections.Hashtable();
+			hashtable.Add(key: "Key1", value: "Value1");
+			hashtable.Add(key: "Key2", value: "Value2");
+
+			Logger.LogCritical(exception: exception, message: "Fatal 1", parameters: hashtable);
+			// **************************************************
+
+			string message = "Hello, World!";
+
+			return Ok(value: message);
+		}
+	}
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,52 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddHttpContextAccessor();
+
+// Note:
+
+//services.AddTransient<Logging.ILogger, Logging.NLogAdapter>(); // Compile Error!
+
+//services.AddTransient<Logging.ILogger<>, Logging.NLogAdapter<>>(); // Compile Error!
+
+//services.AddSingleton
+
+//	(serviceType: typeof(Logging.ILogger<>),
+
+//	implementationType: typeof(Logging.NLogAdapter<>)); // Runtime Error!
+
+
+
+builder.Services.AddTransient
+    (serviceType: typeof(Logging.ILogger<>),
+    implementationType: typeof(Logging.NLogAdapter<>));
+
+//services.AddTransient
+
+//	(serviceType: typeof(Logging.ILogger<>),
+
+//	implementationType: typeof(Logging.Log4NetAdapter<>));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
